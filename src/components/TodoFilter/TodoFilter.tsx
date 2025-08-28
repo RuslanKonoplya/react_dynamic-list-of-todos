4a148fa8d3b42e2102184614f957adfbb3b17b17
Ruslan <ruslan.konoplya@gmail.com>
2025-08-28 15:29:17 +0300

    add solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,8 +1,41 @@
-export const TodoFilter = () => (
+import { useEffect, useState } from "react";
+
+
+type SortFilter = {
+  select: string;
+  textSearch: string;
+};
+
+type Props = {
+ setSortFilter: React.Dispatch<React.SetStateAction<SortFilter>>;
+};
+
+export const TodoFilter: React.FC<Props> = ({ setSortFilter }) => {
+
+  const [textSearch, setTextSearch] = useState('');
+  const [select, setSelect] = useState('');
+
+
+
+  useEffect(() => {
+
+    setSortFilter({ select, textSearch });
+
+  },[select,textSearch])
+
+
+  return (
+
   <form className="field has-addons">
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
+          <select data-cy="statusSelect"
+            value={select}
+            onChange={(e) => {
+              setSelect(e.target.value);
+            }}
+
+          >
           <option value="all">All</option>
           <option value="active">Active</option>
           <option value="completed">Completed</option>
@@ -10,21 +43,42 @@ export const TodoFilter = () => (
       </span>
     </p>
 
+
+
+
+
     <p className="control is-expanded has-icons-left has-icons-right">
       <input
         data-cy="searchInput"
         type="text"
         className="input"
-        placeholder="Search..."
+          placeholder="Search..."
+          value={textSearch}
+          onChange={(e)  => {
+            setTextSearch(e.target.value)
+          }}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
       </span>
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+
+
+        {textSearch && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
         {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
+            <button data-cy="clearSearchButton" type="button" className="delete"
+              onClick={() => {
+                setTextSearch('');
+        }}    />
       </span>
+
+
+        )
+        }
+
+
     </p>
-  </form>
-);
+    </form>
+  )
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,6 +1,22 @@
-import React from 'react';
 
-export const TodoList: React.FC = () => (
+import { Todo } from '../../types/Todo';
+
+
+
+type Props = {
+  todos: Todo[];
+setModalIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setSelectTodo: React.Dispatch<React.SetStateAction<Todo | undefined>>
+    modalIsOpen : boolean
+}
+
+export const TodoList: React.FC<Props> = ({ todos,setModalIsOpen,setSelectTodo,modalIsOpen}) => {
+
+
+
+
+  return(
+
   <table className="table is-narrow is-fullwidth">
     <thead>
       <tr>
@@ -16,36 +32,86 @@ export const TodoList: React.FC = () => (
     </thead>
 
     <tbody>
-      <tr data-cy="todo" className="">
-        <td className="is-vcentered">1</td>
-        <td className="is-vcentered" />
-        <td className="is-vcentered is-expanded">
-          <p className="has-text-danger">delectus aut autem</p>
-        </td>
-        <td className="has-text-right is-vcentered">
-          <button data-cy="selectButton" className="button" type="button">
-            <span className="icon">
-              <i className="far fa-eye" />
-            </span>
-          </button>
-        </td>
-      </tr>
-      <tr data-cy="todo" className="has-background-info-light">
-        <td className="is-vcentered">2</td>
-        <td className="is-vcentered" />
-        <td className="is-vcentered is-expanded">
-          <p className="has-text-danger">quis ut nam facilis et officia qui</p>
-        </td>
-        <td className="has-text-right is-vcentered">
-          <button data-cy="selectButton" className="button" type="button">
-            <span className="icon">
-              <i className="far fa-eye-slash" />
-            </span>
-          </button>
-        </td>
-      </tr>
 
-      <tr data-cy="todo" className="">
+        {todos.map((todo) => (
+
+
+
+
+
+
+            <tr data-cy="todo" className="" key={todo.id}>
+              <td className="is-vcentered">{todo.id}</td>
+
+
+              {todo.completed ? (
+                <td className="is-vcentered">
+              <span className="icon" data-cy="iconCompleted">
+              <i className="fas fa-check" />
+             </span>
+             </td>
+            ) : (<td className="is-vcentered" />) }
+
+
+
+              <td className="is-vcentered is-expanded">
+
+                <p className={todo.completed ? ("has-text-success") :
+                  ("has-text-danger")
+                 }>
+                  {todo.title}</p>
+
+
+
+              </td>
+
+
+              <td className="has-text-right is-vcentered">
+              <button data-cy="selectButton" className="button" type="button"
+                onClick={() => {
+                  setModalIsOpen(true)
+                  setSelectTodo(todo)
+                }}
+              >
+                <span className="icon">
+
+
+                  <i className={modalIsOpen? "far fa-eye-slash" : "far fa-eye"  } />
+                  </span>
+                </button>
+              </td>
+            </tr>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        ))}
+
+
+
+
+
+    </tbody>
+    </table>
+  )
+};
+
+
+
+/*
+
+
+<tr data-cy="todo" className="">
         <td className="is-vcentered">1</td>
         <td className="is-vcentered" />
         <td className="is-vcentered is-expanded">
@@ -95,6 +161,23 @@ export const TodoList: React.FC = () => (
           </button>
         </td>
       </tr>
-    </tbody>
-  </table>
-);
+
+
+
+
+       <tr data-cy="todo" className="has-background-info-light">
+              <td className="is-vcentered">2</td>
+              <td className="is-vcentered" />
+              <td className="is-vcentered is-expanded">
+                <p className="has-text-danger">quis ut nam facilis et officia qui</p>
+              </td>
+              <td className="has-text-right is-vcentered">
+                <button data-cy="selectButton" className="button" type="button">
+                  <span className="icon">
+                    <i className="far fa-eye-slash" />
+                  </span>
+                </button>
+              </td>
+            </tr>
+
+      */
